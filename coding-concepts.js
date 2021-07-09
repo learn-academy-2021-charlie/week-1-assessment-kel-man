80bc8a37490c36ffc7bb5a185c4cc65ec4db3e45
kel-man <kyyafuso@gmail.com>
2021-07-09 15:27:36 -0700

    completed the coding-concepts with commentary on functions being invoked

--- coding-concepts.js
+++ coding-concepts.js
@@ -10,8 +10,8 @@
 var cohort = "Charlie 2021"
 // console.log(cohort.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will log 12
+// b) Verify and explain: This logs the length of the string, 12. This includes the space
 
 
 // --------------------2) What will this log?
@@ -19,8 +19,8 @@ var cohort = "Charlie 2021"
 var greeting = "Hello World!"
 // console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: Wthis will log "l"
+// b) Verify and explain: This logs "l" because it is the character in the third index of "Hello World!"
 
 
 // --------------------3) What will this log?
@@ -29,8 +29,8 @@ var languages = ["JavaScript", "Ruby", "Python", "C++"]
 var index = 1
 // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will log "Ruby"
+// b) Verify and explain: This logs "Ruby" as the value of index is 1, making languages[1] and thus "Ruby"
 
 
 // --------------------4) What will this log?
@@ -38,8 +38,8 @@ var index = 1
 var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I don't think this works because weekendDays is not a string, but we will see
+// b) Verify and explain: I am correct and this console.log throws an error. .toUpperCase() only works on string type data
 
 
 // --------------------5) What will this log?
@@ -47,5 +47,6 @@ var weekendDays = ["saturday", "sunday"]
 var dataTypes = ["number", "string", "Boolean"]
 // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: This will log "number"
+// b) Verify and explain: The dataTypes.length resolves first as it is a single expression, so typeof being called on the
+//                        number 5, 5 being the length of "array".
